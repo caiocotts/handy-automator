d0157bcdae7eac6b30b2b21337c15e9ea930e08c
Caio Cotts <caio@cotts.com.br>
2026-02-20 11:05:44 -0500

    rename DeviceRepository receiver

--- decision-maker/service/device/service.go
+++ decision-maker/service/device/service.go
@@ -11,22 +11,22 @@ type Service struct {
 	deviceRepository persistence.DeviceRepository
 }
 
-func NewService(dr persistence.DeviceRepository) *Service {
-	return &Service{deviceRepository: dr}
+func NewService(r persistence.DeviceRepository) *Service {
+	return &Service{deviceRepository: r}
 }
 
-func (ds Service) Create(ctx context.Context, ip net.IP) (model.Device, error) {
-	return ds.deviceRepository.Create(ctx, ip)
+func (s Service) Create(ctx context.Context, ip net.IP) (model.Device, error) {
+	return s.deviceRepository.Create(ctx, ip)
 }
 
-func (ds Service) Delete(ctx context.Context, id string) error {
-	return ds.deviceRepository.Delete(ctx, id)
+func (s Service) Delete(ctx context.Context, id string) error {
+	return s.deviceRepository.Delete(ctx, id)
 }
 
-func (ds Service) GetById(ctx context.Context, id string) (model.Device, error) {
-	return ds.deviceRepository.Get(ctx, id)
+func (s Service) GetById(ctx context.Context, id string) (model.Device, error) {
+	return s.deviceRepository.Get(ctx, id)
 }
 
-func (ds Service) GetAll(ctx context.Context) ([]model.Device, error) {
-	return ds.deviceRepository.GetAll(ctx)
+func (s Service) GetAll(ctx context.Context) ([]model.Device, error) {
+	return s.deviceRepository.GetAll(ctx)
 }
