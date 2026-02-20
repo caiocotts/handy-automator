4be647eb20f7f568f9385e58de99676cb4503d85
Caio Cotts <caio@cotts.com.br>
2026-02-20 10:05:36 -0500

    wip: endpoint implementation

--- decision-maker/persistence/device_repository.go
+++ decision-maker/persistence/device_repository.go
@@ -9,6 +9,7 @@ import (
 type DeviceRepository interface {
 	Create(ctx context.Context, ip net.IP) (model.Device, error)
 	Get(ctx context.Context, id string) (model.Device, error)
+	GetAll(ctx context.Context) ([]model.Device, error)
 	Update() (model.Device, error)
 	Delete(ctx context.Context, id string) error
 }
