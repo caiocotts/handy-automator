d8988d3655d3314d15e84b86ec2915daae003f63
Caio Cotts <caio@cotts.com.br>
2026-02-20 11:06:35 -0500

    add workflow service and endpoint for creating workflows

---
+++ decision-maker/service/workflow/service.go
@@ -0,0 +1,19 @@
+package workflow
+
+import (
+	"context"
+	"decisionMaker/model"
+	"decisionMaker/persistence"
+)
+
+type Service struct {
+	workflowRepository persistence.WorkflowRepository
+}
+
+func NewService(r persistence.WorkflowRepository) *Service {
+	return &Service{workflowRepository: r}
+}
+
+func (s Service) Create(ctx context.Context, name string) (model.Workflow, error) {
+	return s.workflowRepository.Create(ctx, name)
+}
