3558f088a9d3c495cd802fa00d01b8886cf79f64
Babaruser37 <christianumbertogarcia@gmail.com>
2026-03-10 13:11:05 -0400

    Added get ping function

--- python-processing/api_interface.py
+++ python-processing/api_interface.py
@@ -1,4 +1,20 @@
 import requests
 import json
 
+def get_public_ipv4():
+    try:
+        # Use an external API that returns only the IP address
+        response = requests.get('https://api.ipify.org/?format=json')
+        if response.status_code == 200:
+            return response.json()['ip']
+        else:
+            return f"Error: {response.status_code}"
+    except requests.exceptions.RequestException as e:
+        return f"An error occurred: {e}"
+
+
+def ping(ip_address=get_public_ipv4(), port="3000"):
+    r = requests.get("http://" + ip_address + ":" + port + "/api/ping")
+    print(r.text)
+    return r.status_code == 200
 
