142072b626c4b444cee178c9236492a930161f29
Caio Cotts <caio@cotts.com.br>
2026-03-12 14:29:53 -0400

    fix migration remove broken legacy begin and end statements

--- db/migrations/20260312030621_face_embedding.sql
+++ db/migrations/20260312030621_face_embedding.sql
@@ -1,11 +1,7 @@
 -- +goose Up
--- +goose Statement Begin
 alter table if exists "user"
     add column face_embedding text;
--- +goose StatementEnd
-
 
 -- +goose Down
--- +goose StatementBegin
 alter table if exists "user"
     drop column face_embedding;
