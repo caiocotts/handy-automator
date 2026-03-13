b6a6bf3b31c37f6cef69a100b98fed4a9092c0eb
Caio Cotts <caio@cotts.com.br>
2026-03-13 14:17:08 -0400

    fix typos

--- decision-maker/persistence/user_repository.go
+++ decision-maker/persistence/user_repository.go
@@ -7,7 +7,7 @@ import (
 
 type UserRepository interface {
 	Create(ctx context.Context, username, password string) (model.User, error)
-	Get(ctx context.Context, username string) (model.User, error)
+	Get(ctx context.Context, id string) (model.User, error)
 	GetByUsername(ctx context.Context, username string) (model.User, error)
 	UpdateRefreshToken(ctx context.Context, id, refreshToken string) error
 	Delete(ctx context.Context, id string) error
