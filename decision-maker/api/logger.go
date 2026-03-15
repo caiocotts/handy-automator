b9c0f226371415d9322f7a5b7653a0ae69fb7dfc
Caio Cotts <caio@cotts.com.br>
2026-03-15 18:39:12 -0400

    fix logger overwriting error before logging

--- decision-maker/api/logger.go
+++ decision-maker/api/logger.go
@@ -1,17 +1,13 @@
 package api
 
 import (
-	"log"
 	"log/slog"
 
 	gonanoid "github.com/matoous/go-nanoid/v2"
 )
 
 func logWithRef(err error, op string) string {
-	refCode, err := gonanoid.New(21)
-	if err != nil {
-		log.Fatal("error: failed to generate reference code for log message: ", err)
-	}
+	refCode, _ := gonanoid.New(21)
 
 	slog.Error("internal server error",
 		"operation", op,
