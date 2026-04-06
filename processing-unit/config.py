d20f3d61984cebe15623f3dd0927d1ea5620940e
Babaruser37 <christianumbertogarcia@gmail.com>
2026-04-06 09:50:42 -0400

    Implemented system agnostic camera finding

--- processing-unit/config.py
+++ processing-unit/config.py
@@ -1,5 +1,6 @@
 import cv2
 from cv2_enumerate_cameras import enumerate_cameras
+import platform
 
 
 def get_available_cameras()-> dict[int, str]:
@@ -9,10 +10,26 @@ def get_available_cameras()-> dict[int, str]:
     Returns: 
         - Dict {int Index : str Camera Name}
     """
+    os = platform.system()
+
     available_cameras = {}
-    for camera_info in enumerate_cameras(cv2.CAP_MSMF):
-        available_cameras[camera_info.index] = camera_info.name
-    return available_cameras
+    if os == "Windows":
+        for camera_info in enumerate_cameras(cv2.CAP_MSMF):
+            if camera_info is not None:
+                available_cameras[camera_info.index] = camera_info.name
+        return available_cameras
+    
+    elif os == "Linux":
+        for camera_info in enumerate_cameras(cv2.CAP_V4L2):
+            if camera_info is not None:
+                available_cameras[camera_info.index] = camera_info.name
+        return available_cameras
+    
+    elif os == "Darwin":
+        for camera_info in enumerate_cameras(cv2.CAP_AVFOUNDATION):
+            if camera_info is not None:
+                available_cameras[camera_info.index] = camera_info.name
+        return available_cameras
 
 def get_ptz_camera()-> int|None:
     """
@@ -26,7 +43,6 @@ def get_ptz_camera()-> int|None:
     for keys in available:
         if "vaddio" in available[keys].lower() or "roboshot" in available[keys].lower():
             return keys
-    # Change to None when development is done
     return 0
 
 
